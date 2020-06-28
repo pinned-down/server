2b127f15222f19b6101d3ec80aa2453e85f19a14
Nick Prühs <dev@npruehs.de>
2020-06-28 02:44:34 +0200

    Add example web socket server implementation.

---
+++ src/main/java/de/pinneddown/server/messages/GreetingMessage.java
@@ -0,0 +1,17 @@
+package de.pinneddown.server.messages;
+
+public class GreetingMessage {
+    private String content;
+
+    public GreetingMessage() {
+    }
+
+    public GreetingMessage(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
