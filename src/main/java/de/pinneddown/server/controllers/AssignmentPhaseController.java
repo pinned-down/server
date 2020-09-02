4b4be3a53bcaa9c1fe01a6eb5ccc53f2e358a150
Nick Prühs <dev@npruehs.de>
2020-09-03 01:50:33 +0200

    Allow players to finish assignments.

--- src/main/java/de/pinneddown/server/controllers/AssignmentPhaseController.java
+++ src/main/java/de/pinneddown/server/controllers/AssignmentPhaseController.java
@@ -4,6 +4,8 @@ import de.pinneddown.server.ActionType;
 import de.pinneddown.server.EventManager;
 import de.pinneddown.server.PlayerManager;
 import de.pinneddown.server.actions.AssignStarshipAction;
+import de.pinneddown.server.actions.EndAssignmentPhaseAction;
+import de.pinneddown.server.actions.EndMainPhaseAction;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
@@ -17,5 +19,10 @@ public class AssignmentPhaseController extends WebSocketController {
     @MessageMapping("/assignStarship")
     public void assignStarship(SimpMessageHeaderAccessor headerAccessor, AssignStarshipAction message) {
         relayActionToGame(ActionType.ASSIGN_STARSHIP, message, headerAccessor);
+    }
+
+    @MessageMapping("/endAssignmentPhase")
+    public void endAssignmentPhase(SimpMessageHeaderAccessor headerAccessor, EndAssignmentPhaseAction message) {
+        relayActionToGame(ActionType.END_ASSIGNMENT_PHASE, message, headerAccessor);
     }
 }
